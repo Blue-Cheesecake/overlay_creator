d0b3694ecea588ae81fc03f207bbdb97ecf63834
SINUT WATTANARPORN <s_w.piepie@outlook.com.au>
2024-02-27 22:22:20 +0700

    feat: OVLC-0 core

--- lib/src/overlay_creator.dart
+++ lib/src/overlay_creator.dart
@@ -1,1 +1,24 @@
-mixin OverlayCreator {}
+import 'package:flutter/material.dart';
+
+mixin OverlayCreator {
+  @mustCallSuper
+  @protected
+  OverlayEntry? overlayEntry;
+
+  @protected
+  void displayOverlay({required BuildContext context, required Widget child, VoidCallback? callback}) {
+    assert(overlayEntry == null);
+    overlayEntry = OverlayEntry(
+      builder: (context) => child,
+    );
+    Overlay.of(context).insert(overlayEntry!);
+    if (callback != null) callback();
+  }
+
+  @protected
+  void removeOverlay({VoidCallback? callback}) {
+    overlayEntry?.remove();
+    overlayEntry = null;
+    if (callback != null) callback();
+  }
+}
